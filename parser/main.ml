06792467baa3d5bb60eeef0b554ddd3ac39aa6d6
MamaliKaterina <katerina2398@gmail.com>
2020-04-06 10:11:59 +0300

    Katerina

--- parser/main.ml
+++ parser/main.ml
@@ -1,10 +1,8 @@
 let main =
-  let lexbuf = Lexing.from_channel stdin in
-  let myprogram lexbuf = 
+  let lexbuf = Lexing.from_channel stdin in 
   	try
     	Tony_parser.program Tony_lexer.lexer lexbuf;
     	exit 0
   	with Parsing.Parse_error ->
     	Printf.eprintf "syntax error in line %d\n" lexbuf.Lexing.lex_curr_p.Lexing.pos_lnum;
     	exit 1
-	in myprogram lexbuf
