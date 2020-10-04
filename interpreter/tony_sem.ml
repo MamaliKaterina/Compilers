0456b6aafbea799f6a5db892c68f4dbe1d4fcbac
vpet98 <vasilis4998@gmail.com>
2020-10-04 18:50:02 +0300

    Fixing errors

--- interpreter/tony_sem.ml
+++ interpreter/tony_sem.ml
@@ -125,9 +125,10 @@ and sem_expr ast =
   | E_cons (e1, e2, line)       -> let v1 = sem_expr e1
                                    and v2 = sem_expr e2 in
                                    (match v2 with
-                                    | TY_list(v) -> if v <> v1 then (error "type of list of right operand of operator '#' \
-                                                                            must be of same type as left operand";
-                                                                     raise (TypeError line))
+                                    | TY_list(v) -> if v <> v1 && v <> Null then
+                                                      (error "type of list of right operand of operator '#' \
+                                                       must be either of same type as left operand or empty list";
+                                                       raise (TypeError line))
                                                     else v2
                                     | _          -> error "operator '#' expected a valid type and a list as operands";
                                                      raise (TypeError line) )
@@ -143,14 +144,25 @@ and sem_expr ast =
                                                     raise (TypeError line) )
 
 and check_param exs pars fname line =
+  (*parameters by reference have to be lvalues; we do not check this!*)
   match exs, pars with
   | ([], [])       -> ()
   | (e::te, p::tp) -> let t = sem_expr e in
                       (match p.entry_info with
                        | ENTRY_parameter(pi) -> if pi.parameter_type <> t then
-                                                  (error "parameter type in call of function '%s' inconsistent \
-                                                          with type in function definition" fname;
-                                                   raise (TypeError line))
+                                                (match pi.parameter_type, t with
+                                                 | TY_list(sth), TY_list Null -> ()
+                                                 | _                          -> (error "parameter type in call of function \
+                                                                                         '%s' inconsistent with type \
+                                                                                         in function definition" fname;
+                                                                                  raise (TypeError line)) )
+                                                else if pi.parameter_mode = PASS_BY_REFERENCE then
+                                                  (match e with
+                                                   | E_atom(A_var(v, l))      -> ()
+                                                   | E_atom(A_atom(a, ex, l)) -> ()
+                                                   | _                        -> error "parameter passed by reference has to be \
+                                                                                        an lvalue in call of function '%s'" fname;
+                                                                                 raise (TypeError line) )
                                                 else check_param te tp fname line
                        | _                   -> raise UnknownError )
   | _              -> error "fewer or more parameters than expected in call of function '%s'" fname;
@@ -166,9 +178,9 @@ and sem_call c =
                                                          check_param exprs f.function_paramlist nm line;
                                                          f.function_result
                                                        end
-                                                       else (error "function '%s' in not yet defined" nm;
+                                                       else (error "function '%s' is not yet defined" nm;
                                                              raise (TypeError line))
-                                | _ -> error "identifier '%s' in not a function" nm;
+                                | _ -> error "identifier '%s' is not a function" nm;
                                        raise (TypeError line) )
 
 and sem_atom ast =
@@ -187,9 +199,9 @@ and sem_atom ast =
   | A_atom (a, e, line) -> let v = sem_atom a
                            and n = sem_expr e in
                            (match v, n with
-                            | (TY_array(t), TY_int) -> t
-                            | _                     -> error "identifier is not an array"; print_typ v; (*print_typ only for debugging*)
-                                                       raise (TypeError line) )
+                            | TY_array(t), TY_int -> t
+                            | _                   -> error "identifier is not an array"; (*print_typ v; print_typ //only for debugging*)
+                                                     raise (TypeError line) )
   | A_call c            -> sem_call c
 
 and sem_simple ast =
@@ -197,9 +209,13 @@ and sem_simple ast =
   | S_skip ()             -> ()
   | S_assign (a, e, line) -> let x = sem_atom a
                              and y = sem_expr e in (*it will return the typ of y*)
-                             if x <> y then (error "lvalue and rvalue in assignment are not of the same type";
-                                             raise (TypeError line))
-                             (*else Printf.eprintf "%d " line; print_typ x; //else statement only for debugging*)
+                             if x <> y then
+                               (match x, y with
+                               | TY_list(sth), TY_list Null -> ()
+                               | _                          -> (error "lvalue and rvalue in assignment are not of the same type";
+                                                        (*Printf.eprintf "%d " line; print_typ x; print_typ y;//only for debugging*)
+                                                                raise (TypeError line)) )
+                            (*else Printf.eprintf "%d " line; print_typ x; print_typ y;//else statement only for debugging*)
   | S_call c              -> let v = sem_call c	in
                              if v <> Null then raise UnknownError
 
